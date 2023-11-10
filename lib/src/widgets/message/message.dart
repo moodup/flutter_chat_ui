0b113057532724b74723497ffa63e27b7ae97ccd
Maciej Banaszyński <maciej.b@moodup.team>
2023-11-10 12:41:17 +0100

    Change user id to user model

--- lib/src/widgets/message/message.dart
+++ lib/src/widgets/message/message.dart
@@ -61,7 +61,7 @@ class Message extends StatelessWidget {
 
   /// This is to allow custom user avatar builder
   /// By using this we can fetch newest user info based on id.
-  final Widget Function(String userId)? avatarBuilder;
+  final Widget Function(types.User author)? avatarBuilder;
 
   /// Customize the default bubble using this function. `child` is a content
   /// you should render inside your bubble, `message` is a current message
